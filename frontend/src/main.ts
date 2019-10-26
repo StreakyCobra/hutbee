17268dbd075756ed1eb33d50b59f86ed060ba639
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-10-26 13:00:25 +0200

    Simplify homepage and test connection with backend API

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+(Vue as any).http.options.root = process.env.VUE_APP_API_ROOT;
 
 new Vue({
   router,
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1>Welcome</h1>
+    <p>What is your name?</p>
+    <input v-model="name" type="text" />
+    <div>Answer from the API: {{answer}}</div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
+<script lang="ts">
+import { Component, Watch, Vue } from "vue-property-decorator";
+
+@Component
+export default class Hello extends Vue {
+  name: string = ""
+  answer: string = ""
+
+  mounted() {
+    this.call_api(this.name);
   }
-};
+
+  @Watch("name")
+  onPropertyChanged(value: string, oldValue:string) {
+    this.call_api(value);
+  }
+
+  call_api(value: string) {
+    var url = ""
+    if (value) {
+      url += '?name=' + this.name
+    }
+    this.$http.get(url).then(response => {
+        return response.text();
+      }, response => {
+        this.answer = "error";
+      }).then(response => {
+        this.answer = response || "API call failed"
+      });
+      }
+}
 </script>
