3c0bc6409cfcca01b1a2ef5f1b4c2cb6dab72de4
Fabien Dubosson <fabien.dubosson@gmail.com>
2019-11-08 17:36:45 +0100

    Run linter on frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,39 @@
 <template>
-    <div id="app">
-        <router-view v-if="authenticated || isAuth() "/>
-    </div>
+  <div id="app">
+    <router-view v-if="authenticated || isAuth()" />
+  </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import * as auth from "@/lib/auth";
+import { Component, Vue } from "vue-property-decorator";
+import * as auth from "@/lib/auth";
 
-    @Component
-    export default class App extends Vue {
-        authenticated: boolean = false;
+@Component
+export default class App extends Vue {
+  authenticated: boolean = false;
 
-        mounted() {
-            this.checkAuth();
-            this.$router.afterEach(() => {
-                this.checkAuth();
-            });
-        }
+  mounted() {
+    this.checkAuth();
+    this.$router.afterEach(() => {
+      this.checkAuth();
+    });
+  }
 
-        isAuth() {
-            return this.$route.name == 'auth';
-        }
+  isAuth() {
+    return this.$route.name == "auth";
+  }
 
-        checkAuth() {
-            auth.isConnected(this.$http,
-                () => {
-                    this.authenticated = true;
-                },
-                () => {
-                    this.authenticated = false;
-                    this.$router.push({ name: "auth" });
-                }
-            );
-        }
-    }
+  checkAuth() {
+    auth.isConnected(
+      this.$http,
+      () => {
+        this.authenticated = true;
+      },
+      () => {
+        this.authenticated = false;
+        this.$router.push({ name: "auth" });
+      }
+    );
+  }
+}
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,31 +1,30 @@
 <template>
-    <div>
-        <h1>Welcome</h1>
-        <p>Hi, {{ username }}</p>
-        <input type="button" value="Logout" @click="logout"/>
-    </div>
+  <div>
+    <h1>Welcome</h1>
+    <p>Hi, {{ username }}</p>
+    <input type="button" value="Logout" @click="logout" />
+  </div>
 </template>
 
-
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import * as auth from "@/lib/auth"
+import { Component, Vue } from "vue-property-decorator";
+import * as auth from "@/lib/auth";
 
-    @Component
-    export default class Hello extends Vue {
-        username: string = "";
+@Component
+export default class Hello extends Vue {
+  username: string = "";
 
-        mounted() {
-            this.$http.get('me').then(response => {
-                response.text().then(data => {
-                    this.username = data;
-                });
-            })
-        }
+  mounted() {
+    this.$http.get("me").then(response => {
+      response.text().then((data: string) => {
+        this.username = data;
+      });
+    });
+  }
 
-        logout() {
-            auth.logout();
-            this.$router.push({"name": "auth"});
-        }
-    }
+  logout() {
+    auth.logout();
+    this.$router.push({ name: "auth" });
+  }
+}
 </script>
