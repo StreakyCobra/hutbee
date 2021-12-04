a6988d35d74661cf00d0f0549136a8e7548036be
Fabien Dubosson <fabien.dubosson@gmail.com>
2021-12-04 01:10:53 +0100

    Remove notifications from feeder

--- backend/hutbee/workers/feeder.py
+++ backend/hutbee/workers/feeder.py
@@ -13,10 +13,9 @@ from hutbee.db import DB
 
 
 class Worker(ConsumerMixin):
-    def __init__(self, connection, queue, notifier):
+    def __init__(self, connection, queue):
         self.connection = connection
         self.queue = queue
-        self.notifier = notifier
 
     def get_consumers(self, Consumer, channel):
         return [
@@ -29,24 +28,8 @@ class Worker(ConsumerMixin):
     def store_measurement(self, body: Any, message: kombu.Message):
         """Store a measurement in the database."""
         DB[config.MEASUREMENTS_COL].insert_one(body)
-        self.notifier.publish(
-            {"username": "fabien", "message": body["values"]["temperature"]}
-        )
         logger.info(f"Measurement processed")
         message.ack()
-
-
-def setup_notifier():
-    user = os.environ["BACKEND_RABBITMQ_USERNAME"]
-    password = os.environ["BACKEND_RABBITMQ_PASSWORD"]
-    uri = f"amqp://{user}:{password}@backend-rabbitmq:5672"
-    with kombu.Connection(
-        uri, transport_options={"confirm_publish": True}
-    ) as connection:
-        exchange = kombu.Exchange("notify")(connection)
-        exchange.declare()
-        producer = connection.Producer(exchange=exchange)
-        return producer
 
 
 def main():
@@ -57,15 +40,13 @@ def main():
     uri = f"amqp://{user}:{password}@controller:{port}"
     queue = kombu.Queue("measurements.persist")
 
-    notifier = setup_notifier()
-
     while True:
         try:
             with kombu.Connection(uri) as connection:
                 with connection.channel() as channel:
                     queue.declare(channel=channel)
                     queue.bind_to("measurements", channel=channel)
-                worker = Worker(connection, queue, notifier)
+                worker = Worker(connection, queue)
                 worker.run()
         except ConnectionError:
             logger.warning("Connection to rabbitmq failed")
