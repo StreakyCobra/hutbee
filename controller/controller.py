bbdf0d4cead489eed51c48910f6a585cccae2aae
Fabien Dubosson <fabien.dubosson@gmail.com>
2022-08-08 21:35:45 +0200

    Asynchronous Modbus worker through ZMQ

---
+++ controller/controller.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+from contextlib import contextmanager
+from threading import Thread
+
+import zmq
+from flask import Flask, jsonify, make_response
+from pymodbus.client.sync import ModbusTcpClient
+
+APP = Flask(__name__)
+CONTEXT = zmq.Context()
+
+
+class ModbusWorker(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.client = ModbusTcpClient(os.environ["MODBUS_SERVER_HOST"])
+        self.socket = CONTEXT.socket(zmq.REP)
+
+    def run(self):
+        self.socket.bind("inproc://stream")
+        while True:
+            args = self.socket.recv_json()
+            result = self.execute(args)
+            self.socket.send_json(result)
+
+    def execute(self, args):
+        command = args.get("command")
+        if command == "heating_status":
+            return self.heating_status()
+        elif command == "turn_heating_on":
+            return self.turn_heating_on()
+        elif command == "turn_heating_off":
+            return self.turn_heating_off()
+        else:
+            return {"content": {"message": "Unknown command"}, "status": 400}
+
+    def heating_status(self):
+        try:
+            coils = client.read_coils(80, 8).bits
+            heater = "ON" if coils[1] else "OFF"
+            pump = "ON" if coils[2] else "OFF"
+            return {"content": {"heater": heater, "pump": pump}, "status": 200}
+        except:
+            return {
+                "content": {"message": "Can **NOT** access the heating status"},
+                "status": 400,
+            }
+
+    def turn_heating_on(self):
+        try:
+            client.write_coil(1, 1)
+            return {
+                "content": {"message": "The heating has been turned **ON**"},
+                "status": 200,
+            }
+        except:
+            return {
+                "content": {"message": "Can **NOT** turn the heating on"},
+                "status": 400,
+            }
+
+    def turn_heating_off(self):
+        try:
+            client.write_coil(1, 0)
+            return {
+                "content": {"message": "The heating has been turned **OFF**"},
+                "status": 200,
+            }
+        except:
+            return {
+                "content": {"message": "Can **NOT** turn the heating off"},
+                "status": 400,
+            }
+
+
+@contextmanager
+def channel():
+    socket = CONTEXT.socket(zmq.REQ)
+    socket.connect("inproc://stream")
+    try:
+        yield socket
+    finally:
+        socket.close()
+
+
+@APP.route("/heating", methods=["GET"])
+def heating_status():
+    """Turn the heating on."""
+    with channel() as socket:
+        socket.send_json({"command": "heating_status"})
+        response = socket.recv_json()
+        return make_response(response.content, response.status)
+
+
+@APP.route("/heating/on", methods=["POST"])
+def turn_heating_on():
+    """Turn the heating on."""
+    with channel() as socket:
+        socket.send_json({"command": "turn_heating_on"})
+        response = socket.recv_json()
+        return make_response(response.content, response.status)
+
+
+@APP.route("/heating/off", methods=["POST"])
+def turn_heating_off():
+    """Turn the heating off."""
+    with channel() as socket:
+        socket.send_json({"command": "turn_heating_off"})
+        response = socket.recv_json()
+        return make_response(response.content, response.status)
+
+
+def main():
+    worker = ModbusWorker()
+    worker.start()
+    APP.run(host="0.0.0.0", port="80")
+
+
+if __name__ == "__main__":
+    main()
